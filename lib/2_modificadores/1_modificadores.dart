5939fa592ee349098db90d241d2bbb9bba1ad7a4
oapmartins <octaviomartins10@gmail.com>
2021-10-03 22:05:01 -0300

    Estudando dart

---
+++ lib/2_modificadores/1_modificadores.dart
@@ -0,0 +1,12 @@
+void main() {
+  var nomeCompleto = 'Octávio';
+  print(nomeCompleto);
+
+  // final
+  final nomeCompletoFinal = 'Octávio Teste';
+  final nomeCompletoFinal2 = nomeCompleto;
+
+  // final
+  const nomeCompletoConst = 'Octávio Teste';
+  const nomeCompletoConst2 = nomeCompletoConst;
+}
