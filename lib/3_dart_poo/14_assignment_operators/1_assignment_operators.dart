04de6a500f6cabf9bf651ed42e25d5b87561e818
oapmartins <octaviomartins10@gmail.com>
2021-10-17 15:59:27 -0300

    Estudando sobre assignment operators

---
+++ lib/3_dart_poo/14_assignment_operators/1_assignment_operators.dart
@@ -0,0 +1,16 @@
+String? nome;
+void main() {
+  // =  -=  /=  %=  >>=  ??=
+  // +=  *=  ~/=  <<=  &=  |=
+
+  var numero = 1;
+  numero += 2;
+  print(numero);
+
+  var numero2 = 2.0;
+  numero2 /= 1;
+  print(numero2);
+
+  nome ??= 'Octávio';
+  print(nome);
+}
