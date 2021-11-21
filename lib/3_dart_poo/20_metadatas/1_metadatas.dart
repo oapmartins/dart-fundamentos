17ee59d266252f015d05fa0b3c26a0421893689a
oapmartins <octaviomartins10@gmail.com>
2021-11-21 10:58:34 -0300

    Estudando reflect no dart

--- lib/3_dart_poo/20_metadatas/1_metadatas.dart
+++ lib/3_dart_poo/20_metadatas/1_metadatas.dart
@@ -1,5 +1,4 @@
 import 'dart:mirrors';
-
 import 'fazer.dart';
 import 'pessoa.dart';
 
@@ -11,7 +10,30 @@ void main() {
   classMirror.metadata.forEach((annotation) {
     var instanceAnnotation = annotation.reflectee;
     if (instanceAnnotation is Fazer) {
-      print(instanceAnnotation.quem);
+      print("Quem: ${instanceAnnotation.quem}");
+      print("Oque: ${instanceAnnotation.oque}");
+    }
+  });
+
+  classMirror.declarations.values.forEach((declarationMirror) {
+    if (declarationMirror is VariableMirror) {
+      declarationMirror.metadata.forEach((annotation) {
+        var instanceAnnotation = annotation.reflectee;
+        if (instanceAnnotation is Fazer) {
+          print('Variáveis --------------');
+          print("Quem: ${instanceAnnotation.quem}");
+          print("Oque: ${instanceAnnotation.oque}");
+        }
+      });
+    } else if (declarationMirror is MethodMirror) {
+      declarationMirror.metadata.forEach((annotation) {
+        var instanceAnnotation = annotation.reflectee;
+        if (instanceAnnotation is Fazer) {
+          print('Métodos --------------');
+          print("Quem: ${instanceAnnotation.quem}");
+          print("Oque: ${instanceAnnotation.oque}");
+        }
+      });
     }
   });
 }
--- lib/3_dart_poo/20_metadatas/pessoa.dart
+++ lib/3_dart_poo/20_metadatas/pessoa.dart
@@ -1,4 +1,22 @@
+import 'dart:io';
+
 import 'fazer.dart';
 
-@Fazer(quem: 'Octávio', oque: 'Testando anotações de classe')
-class Pessoa {}
+@Fazer(
+  quem: 'Octávio',
+  oque: 'Testando anotações de classe',
+)
+@gzip
+class Pessoa {
+  @Fazer(
+    quem: 'Octávio no atributo',
+    oque: 'Testando anotações do atributo',
+  )
+  String? nome;
+
+  @Fazer(
+    quem: 'Octávio no MÉTODO',
+    oque: 'Testando anotações do método',
+  )
+  void fazerAlgo() {}
+}
