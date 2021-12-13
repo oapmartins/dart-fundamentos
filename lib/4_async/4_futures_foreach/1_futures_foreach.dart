886a9fd48e8623cb4cfc43f2c5120dd7b051af2b
Octavio Martins <octaviomartins10@gmail.com>
2021-12-13 12:33:09 -0300

    Finalizando estudos future e await

--- lib/4_async/4_futures_foreach/1_futures_foreach.dart
+++ lib/4_async/4_futures_foreach/1_futures_foreach.dart
@@ -3,9 +3,20 @@ import '../../3_dart_poo/14_assignment_operators/1_assignment_operators.dart';
 Future<void> main() async {
   var nomes = ['Octávio', 'Flavio', 'Marcela'];
   print('Inicio');
-  nomes.forEach((nome) async {
+
+  // Fazer esse tipo de iteração não funciona com foreach.
+  // nomes.forEach((nome) async {
+  //   print(await saudacao(nome));
+  // });
+
+  // utilizando future.foreach
+  await Future.forEach<String>(nomes, (nome) async {
     print(await saudacao(nome));
   });
+  // Utilizando FOR
+  for (var nome in nomes) {
+    print(await saudacao(nome));
+  }
   print('Fim');
 }
 
