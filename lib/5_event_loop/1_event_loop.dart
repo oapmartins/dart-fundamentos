f640704aeb22d65e4bcef09526429d8e127eca2f
Octavio Martins <octaviomartins10@gmail.com>
2021-12-20 13:10:21 -0300

    Estudando Event Loop com await

--- lib/5_event_loop/1_event_loop.dart
+++ lib/5_event_loop/1_event_loop.dart
@@ -1,10 +1,16 @@
 import 'dart:async';
 
-void main() {
+Future<void> main() async {
   print('Inicio Main');
   scheduleMicrotask(() => print('Microtask #1'));
-  Future.delayed(Duration(seconds: 1), () => print('Future #1 dalyed'));
-  Future(() => print('Future #2 dalyed'));
-  Future(() => print('Future #3 dalyed'));
+  await Future.delayed(Duration(seconds: 1), () => print('Future #1 dalyed'));
+  Future(() {
+    var i = 0;
+    while (i < 20000) {
+      i++;
+    }
+    print('Future #2 dalyed');
+  });
+  await Future(() => print('Future #3 dalyed'));
   print('Fim Main');
 }
