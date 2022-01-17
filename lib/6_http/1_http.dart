5ca7266fe0f658b3ea32a183328bce8112f1ced6
oapmartins <octaviomartins10@gmail.com>
2022-01-17 20:49:54 -0300

    Estudando sobre chamadas http e tratamento das requisições

--- lib/6_http/1_http.dart
+++ lib/6_http/1_http.dart
@@ -4,7 +4,41 @@ import 'package:http/http.dart' as http;
 Future<void> main() async {
   // buscarCep();
   // buscarPosts();
-  buscarCepErro();
+  // buscarCepErro();
+  // salvarPost();
+  atualizarPost();
+}
+
+Future<void> atualizarPost() async {
+  var url = 'https://jsonplaceholder.typicode.com/posts/1';
+  var mapaRequest = {
+    'id': '1',
+    'title': 'Post novo',
+    'body': 'Descrição do post',
+    'userId': '1',
+  };
+  var response = await http.put(
+    Uri.parse(url),
+    body: convert.jsonEncode(mapaRequest),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  );
+  print(response.statusCode);
+  print(response.body);
+}
+
+Future<void> salvarPost() async {
+  var url = 'https://jsonplaceholder.typicode.com/posts/';
+  var mapaRequest = {
+    'title': 'Post novo',
+    'body': 'Descrição do post',
+    'userId': '1',
+  };
+  var response =
+      await http.post(Uri.parse(url), body: convert.jsonEncode(mapaRequest));
+  print(response.statusCode);
+  print(response.body);
 }
 
 buscarCep() async {
