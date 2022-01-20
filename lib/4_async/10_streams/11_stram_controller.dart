f6c3c1396d91994f985f9068fa654cec94a84161
oapmartins <octaviomartins10@gmail.com>
2022-01-19 22:10:16 -0300

    Finalizando estudos sobre stream controller

--- lib/4_async/10_streams/11_stram_controller.dart
+++ lib/4_async/10_streams/11_stram_controller.dart
@@ -3,24 +3,27 @@ import 'dart:async';
 Future<void> main() async {
   print('Início Stram controller');
 
-  final streamController = StreamController<int>.broadcast();
+  final streamController = StreamController<Pessoa>.broadcast();
 
   final inStream = streamController.sink;
   final outStream = streamController.stream;
 
-  outStream
-      .skip(1)
-      .where((numero) => numero % 2 == 0)
-      .map((numero) => 'O valor par enviado é $numero')
-      .listen((strConvertida) => print(strConvertida));
+  outStream.listen((pessoa) => print('Seja bem vindo ${pessoa.nome}'));
 
   var numeros = List.generate(20, (index) => index);
 
   for (var numero in numeros) {
-    inStream.add(numero);
+    inStream.add(Pessoa(nome: 'Octávio $numero'));
     await Future.delayed(Duration(milliseconds: 500));
   }
 
   print('Fim Stram controller');
   await streamController.close();
+}
+
+class Pessoa {
+  String nome;
+  Pessoa({
+    required this.nome,
+  });
 }
